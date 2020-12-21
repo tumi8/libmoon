244f87926ffda4e3e1de21f6d96c30f3c723591e
Sebastian Gallenmüller <gallenmu@net.in.tum.de>
2020-12-21 04:25:27 +0100

    simplify ice driver

--- lua/driver/ice.lua
+++ lua/driver/ice.lua
@@ -49,34 +49,34 @@ function dev:enableRxTimestampsAllPackets(queue)
 	self:readAndSet(GLTSYN_SYNC_DLAY, 'GLTSYN_SYNC_DLAY', 0x0)
 
 	self:onlyRead(GLTSYN_ENA0, 'GLTSYN_ENA0')
-	self:onlyRead(GLTSYN_ENA1, 'GLTSYN_ENA1')
+	--self:onlyRead(GLTSYN_ENA1, 'GLTSYN_ENA1')
 	self:readAndSet(GLTSYN_ENA0, 'GLTSYN_ENA0', 0x1)
-	self:readAndSet(GLTSYN_ENA1, 'GLTSYN_ENA1', 0x1)
+	--self:readAndSet(GLTSYN_ENA1, 'GLTSYN_ENA1', 0x1)
 
 	local val = dpdkc.read_reg32(self.id, GLTSYN_STAT0)
 	val = bit.band(val, 0xFFFFFF08)
 	self:readAndSet(GLTSYN_STAT0, 'GLTSYN_STAT0', val)
-	local val = dpdkc.read_reg32(self.id, GLTSYN_STAT1)
-	val = bit.band(val, 0xFFFFFF08)
-	self:readAndSet(GLTSYN_STAT1, 'GLTSYN_STAT1', val)
+	--local val = dpdkc.read_reg32(self.id, GLTSYN_STAT1)
+	--val = bit.band(val, 0xFFFFFF08)
+	--self:readAndSet(GLTSYN_STAT1, 'GLTSYN_STAT1', val)
 
 	local val = dpdkc.read_reg32(self.id, GLINT_TSYN_PHY)
 	val = bit.band(val, 0xFFFFFFE0)
 	self:readAndSet(GLINT_TSYN_PHY, 'GLINT_TSYN_PHY', val)
 
-	local val = dpdkc.read_reg32(self.id, PF_SB_REM_DEV_CTL)
-        local val = bit.bor(val, 0x2) -- PF_SB_REM_DEV_CTL_PHY0
-	self:readAndSet(PF_SB_REM_DEV_CTL, 'PF_SB_REM_DEV_CTL', val)
+	--local val = dpdkc.read_reg32(self.id, PF_SB_REM_DEV_CTL)
+        --local val = bit.bor(val, 0x2) -- PF_SB_REM_DEV_CTL_PHY0
+	--self:readAndSet(PF_SB_REM_DEV_CTL, 'PF_SB_REM_DEV_CTL', val)
         
-	self:readAndSet(GLINT_TSYN_PFMSTR0, 'GLINT_TSYN_PFMSTR0', 0x0)
-	self:readAndSet(GLINT_TSYN_PFMSTR1, 'GLINT_TSYN_PFMSTR1', 0x0)
+	--self:readAndSet(GLINT_TSYN_PFMSTR0, 'GLINT_TSYN_PFMSTR0', 0x0)
+	--self:readAndSet(GLINT_TSYN_PFMSTR1, 'GLINT_TSYN_PFMSTR1', 0x0)
 
-	self:onlyRead(GLTSYN_INCVAL_H0, 'GLTSYN_INCVAL_H0')
-	self:onlyRead(GLTSYN_INCVAL_H1, 'GLTSYN_INCVAL_H1')
-	self:readAndSet(GLTSYN_INCVAL_H0, 'GLTSYN_INCVAL_H0', 0xF)
-	self:readAndSet(GLTSYN_INCVAL_L0, 'GLTSYN_INCVAL_L0', 0xF)
-	self:readAndSet(GLTSYN_INCVAL_H1, 'GLTSYN_INCVAL_H1', 0xFFFF)
-	self:readAndSet(GLTSYN_INCVAL_L1, 'GLTSYN_INCVAL_L1', 0xFFFF)
+	--self:onlyRead(GLTSYN_INCVAL_H0, 'GLTSYN_INCVAL_H0')
+	--self:onlyRead(GLTSYN_INCVAL_H1, 'GLTSYN_INCVAL_H1')
+	--self:readAndSet(GLTSYN_INCVAL_H0, 'GLTSYN_INCVAL_H0', 0xF)
+	--self:readAndSet(GLTSYN_INCVAL_L0, 'GLTSYN_INCVAL_L0', 0xF)
+	--self:readAndSet(GLTSYN_INCVAL_H1, 'GLTSYN_INCVAL_H1', 0xFFFF)
+	--self:readAndSet(GLTSYN_INCVAL_L1, 'GLTSYN_INCVAL_L1', 0xFFFF)
 	
 	print('written')
 end
