315a614872df88f7b297052b5683b948964d49da
Sebastian Gallenmüller <gallenmu@net.in.tum.de>
2021-01-06 17:37:04 +0100

    update device code

--- src/device.c
+++ src/device.c
@@ -27,10 +27,12 @@ static uint8_t symm_rss_hash_key[SYMM_RSS_HASHKEY_LENGTH] = { 0x6D, 0x5A, 0x6D, 
 static volatile uint8_t* registers[RTE_MAX_ETHPORTS];
 
 uint32_t read_reg32(uint16_t port, uint32_t reg) {
+	printf("%p\n", (void*) registers[port]);
 	return *(volatile uint32_t*)(registers[port] + reg);
 }
 
 void write_reg32(uint16_t port, uint32_t reg, uint32_t val) {
+	printf("%p\n", (void*) registers[port]);
 	*(volatile uint32_t*)(registers[port] + reg) = val;
 }
 
@@ -66,8 +68,7 @@ struct libmoon_device_config {
 };
 
 int dpdk_configure_device(struct libmoon_device_config* cfg) {
-	RTE_LOG(INFO, USER1, "this is a output of a outputty nature");
-	return 100;
+	printf("outputty\n");
 	const char* driver = dpdk_get_driver_name(cfg->port);
 	bool is_i40e_device = strcmp("net_i40e", driver) == 0;
 	struct rte_eth_dev_info dev_info;
@@ -157,7 +158,7 @@ int dpdk_configure_device(struct libmoon_device_config* cfg) {
 		},
 		.offloads = tx_offloads,
 	};
-	return 1;
+	printf("ehereherhere\n");
 	for (int i = 0; i < cfg->tx_queues; i++) {
 		rc = rte_eth_tx_queue_setup(cfg->port, i, cfg->tx_descs ? cfg->tx_descs : DEFAULT_TX_DESCS, SOCKET_ID_ANY, &tx_conf);
 		if (rc) {
@@ -165,7 +166,6 @@ int dpdk_configure_device(struct libmoon_device_config* cfg) {
 			return rc;
 		}
 	}
-	return 2;
 	struct rte_eth_rxconf rx_conf = {
 		.rx_drop_en = cfg->drop_enable,
 		.rx_thresh = {
@@ -175,7 +175,6 @@ int dpdk_configure_device(struct libmoon_device_config* cfg) {
 		},
 		.offloads = rx_offloads,
 	};
-	return 3;
 	for (int i = 0; i < cfg->rx_queues; i++) {
 		rc = rte_eth_rx_queue_setup(cfg->port, i, cfg->rx_descs ? cfg->rx_descs : DEFAULT_RX_DESCS, SOCKET_ID_ANY, &rx_conf, cfg->mempools[i]);
 		if (rc != 0) {
@@ -183,12 +182,11 @@ int dpdk_configure_device(struct libmoon_device_config* cfg) {
 			return rc;
 		}
 	}
-	return 5;
 	rc = rte_eth_dev_start(cfg->port);
 	printf("registers preloading\n");
 	if (RTE_DEV_TO_PCI(dev_info.device)) {
-		printf("registers loading\n");
 		registers[cfg->port] = (uint8_t*) RTE_DEV_TO_PCI(dev_info.device)->mem_resource[0].addr;
+		printf("registers loading %p\n", (void*) registers[cfg->port]);
 	} else {
 		printf("registers null\n");
 		registers[cfg->port] = NULL;
