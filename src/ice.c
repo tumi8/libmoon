b97dc101750c1739fe9a0bed28f19d782777cfd0
Sebastian Gallenmueller <gallenmu@net.in.tum.de>
2021-01-08 17:40:45 +0100

    extend init_timer function() still incomplete

--- src/ice.c
+++ src/ice.c
@@ -21,7 +21,55 @@ void* dpdk_get_ice_dev_hw_addr(int port) {
 
 void ice_init_timer(int port) {
 	printf("ice_init_timer(port=%i) start\n", port);
-	//wr32(hw, GLTSYN_SYNC_DLAY, 0);
+	
+	u32 regval;
+	u8 master_idx;
+
+	master_idx = 0;
+
+	/**
+         *  The following code is based on ice_ptp_init() in ice_ptp.c of non-DPDK ice driver.
+         */
+
+	struct ice_hw *hw = ICE_DEV_PRIVATE_TO_HW(rte_eth_devices[port].data->dev_private);
+	wr32(hw, GLTSYN_SYNC_DLAY, 0);
+	/* Enable master clocks */
+	wr32(hw, GLTSYN_ENA(master_idx), GLTSYN_ENA_TSYN_ENA_M);
+
+	//status = ice_ptp_ena_phy_time_syn_ext(pf);
+
+        regval = rd32(hw, GLTSYN_STAT(master_idx));
+
+        /* Do not touch the reserved bits and set other bits
+         * to zero
+         */
+        regval &= 0xFFFFFF08;
+        wr32(hw, GLTSYN_STAT(master_idx), regval);
+
+        regval = rd32(hw, GLINT_TSYN_PHY);
+        /* Do not touch the reserved bits and set other bits
+         * to zero
+         */
+        regval &= 0xFFFFFFE0;
+        wr32(hw, GLINT_TSYN_PHY, regval);
+
+	//#define PF_SB_REM_DEV_CTL_PHY0  BIT(2)
+        //if (ice_is_generic_mac(hw)) {
+        //      regval = rd32(hw, PF_SB_REM_DEV_CTL);
+        //      regval |= PF_SB_REM_DEV_CTL_PHY0;
+        //      wr32(hw, PF_SB_REM_DEV_CTL, regval);
+        //}
+        /* Write the increment time value to PHY and LAN */
+        status = ice_ptp_set_increment(pf, 0);
+        //if (status)
+        //        goto err_exit;
+        
+        /* Acquire the global hardware lock */
+        //if (!ice_ptp_lock(pf)) {
+        //        status = ICE_ERR_IN_USE;
+        //        goto err_exit;
+        //}
+
 	printf("ice_init_timer(port=%i) stop\n", port);
 	return;
 }
