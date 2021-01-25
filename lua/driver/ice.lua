5a8d19a3fc6d1472d3f6173efe0d82df9bf7a301
stubbe <stubbe@net.in.tum.de>
2021-01-25 21:07:59 +0100

    Add compatibility with examples/hardware-timestamping.lua
    [INFO]  Trying to enable rx timestamping of all packets, this isn't
            supported by most nics
    [INFO]  Trying to generate ~1000 mbit/s
    [WARN]  Per-queue rate limit is not supported on this device, setting
    	per-device rate limit to 1000 Mbit/s instead (note: this may
            fail as well if the NIC doesn't support any rate limiting).
    [WARN]  global rate limiting is not supported by the hardware or driver
    [Device: id=1] RX: 11.80 Mpps, 5666 Mbit/s (7554 Mbit/s with framing)
    [Device: id=0] TX: 12.04 Mpps, 5779 Mbit/s (7705 Mbit/s with framing)
    [Device: id=1] RX: 25.25 Mpps, 12121 Mbit/s (16162 Mbit/s with framing)
    [Device: id=0] TX: 25.25 Mpps, 12121 Mbit/s (16161 Mbit/s with framing)
    [Device: id=1] RX: 25.16 Mpps, 12077 Mbit/s (16103 Mbit/s with framing)
    [Device: id=0] TX: 25.16 Mpps, 12077 Mbit/s (16103 Mbit/s with framing)
    [Device: id=1] RX: 25.27 Mpps, 12129 Mbit/s (16172 Mbit/s with framing)
    [Device: id=0] TX: 25.27 Mpps, 12128 Mbit/s (16170 Mbit/s with framing)
    [Device: id=1] RX: 25.23 Mpps, 12111 Mbit/s (16148 Mbit/s with framing)
    [Device: id=0] TX: 25.23 Mpps, 12112 Mbit/s (16150 Mbit/s with framing)
    [INFO]  Inter-arrival time distribution, this will report 0 on unsupported NICs
    [Device: id=1] RX: 12.73 Mpps, 6111 Mbit/s (8148 Mbit/s with framing)
    [Device: id=0] TX: 12.23 Mpps, 5869 Mbit/s (7825 Mbit/s with framing)
    Samples: 126199304, Average: 39.6 ns, StdDev: 374.4 ns, Quartiles: 6.0/10.0/21.0 ns

--- lua/driver/ice.lua
+++ lua/driver/ice.lua
@@ -5,9 +5,18 @@ local dpdkc = require "dpdkc"
 local ffi   = require "ffi"
 local log   = require "log"
 
-function dev:enableRxTimestampsAllPackets(queue)
+function dev:enableRxTimestampsAllPackets(self)
 	dpdkc.ice_init_timer(self.id)
 end
 
-return dev
+-- yes, this card supports timestamping
+-- (TODO might want to implement something here)
+dev.timeRegisters = {0, 0, 0, 0}
+function dev:enableRxTimestamps(self, udpPort) end
+function dev:enableTxTimestamps(queue) end
+function dev:hasRxTimestamp() return 1 end
+-- not needed(?) but avoids some log spam
+function dev:getTxTimestamp(self, wait) return 0 end
 
+
+return dev
