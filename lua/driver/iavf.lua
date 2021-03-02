1c37984897dc1de6d702d1c3c80298b8f09ad58e
Florian Wiedner <wiedner@net.in.tum.de>
2021-03-02 17:15:46 +0100

    First step to enable timestamping on IAVF driver

--- lua/driver/iavf.lua
+++ lua/driver/iavf.lua
@@ -4,6 +4,12 @@ local dev = {}
 local ffi   = require "ffi"
 local dpdkc = require "dpdkc"
 
-
+dev.timeRegisters = {0, 0, 0, 0}
+function dev:enableRxTimestamps(self, udpPort) end
+function dev:enableTxTimestamps(queue) end
+function dev:hasRxTimestamp() return 1 end
+-- not needed(?) but avoids some log spam
+function dev:getTxTimestamp(self, wait) return 0 end
+function dev:resetTimeCounters() return 1 end
 
 return dev
